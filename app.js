b6082406420d28388874c2db5afd2dbe8c570e9d
jehanzorob <Jehanzorob@hotmail.com>
2022-09-19 04:54:09 -0400

    getting ready to send nodeEX to github-spet 19

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let http=require('http')
 //https://www.youtube.com/watch?v=Q1kHG842HoI
 //https://www.youtube.com/watch?v=yXT1ElMEkW8
+// sept 19
 //&&&&&&&&& to new braanch
 http.createServer(function(req, res){
 res.writeHead(200,{'Content-Type':'text/html'});
